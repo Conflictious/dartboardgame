233cf741f33dcc1ae4c067e710c06e80af5bd125
Conflictious <drtrunks@gmail.com>
2017-06-01 11:35:46 +0200

    Update __init__.py
    added bulls(eye)

--- __init__.py
+++ __init__.py
@@ -3,11 +3,14 @@ from collections import defaultdict
 
 def possiblethrows():
     ptdict = defaultdict(list)
-    for i in range(1,21):
+    for i in range(1, 21):
         ptdict[i].append("single " + str(i))
         ptdict[i*2].append("double " + str(i))
         ptdict[i*3].append("triple " + str(i))
+    ptdict[25].append("bull 25")
+    ptdict[50].append("bullseye 50")
     return ptdict
+
 
 def dartboardgame(points):
     pt = possiblethrows()
@@ -15,14 +18,12 @@ def dartboardgame(points):
     while points != 0:
         throw = max([d for d in pt if d <= points])
         #the last throw has to be a double
-        if points - throw <= 1 and throw not in range(0,40,2):
+        if points - throw <= 1 and not (throw in range(0, 40, 2) or throw == 50):
             throw = max([d for d in pt if d <= points-2])
-            gameplan.append([points,pt[throw][0]])
+            gameplan.append([points, pt[throw][0]])
             points -= throw
         else:
             throw = max([d for d in pt if d <= points])
-            gameplan.append([points,pt[throw][0]])
+            gameplan.append([points, pt[throw][0]])
             points -= throw
     return gameplan
-    
-    
